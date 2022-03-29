80da4f0714c9119dca86c27467e6386b0b07a090
phil2sat <phil2sat@gmail.com>
2022-03-29 13:22:45 +0200

    Reduce Compression level from -9 to -6
    Reduce Compression level from -9 to -6

--- scripts/gen_initramfs_list.sh
+++ scripts/gen_initramfs_list.sh
@@ -242,10 +242,10 @@ case "$arg" in
 		output=${cpio_list}
 		echo "$output_file" | grep -q "\.gz$" && compr="gzip -n -9 -f"
 		echo "$output_file" | grep -q "\.bz2$" && compr="bzip2 -9 -f"
-		echo "$output_file" | grep -q "\.lzma$" && compr="lzma -9 -f"
+		echo "$output_file" | grep -q "\.lzma$" && compr="lzma -6 -f"
 		echo "$output_file" | grep -q "\.xz$" && \
 				compr="xz --check=crc32 --lzma2=dict=1MiB"
-		echo "$output_file" | grep -q "\.lzo$" && compr="lzop -9 -f"
+		echo "$output_file" | grep -q "\.lzo$" && compr="lzop -6 -f"
 		echo "$output_file" | grep -q "\.cpio$" && compr="cat"
 		shift
 		;;
